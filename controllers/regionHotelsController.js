514df16174ef9eb600d9ea5cfd4b1f4d9b1efae6
erolmahmutoglu <erol.mahmutoglu@outlook.com>
2024-11-01 21:29:51 +0300

    wip

--- controllers/regionHotelsController.js
+++ controllers/regionHotelsController.js
@@ -13,12 +13,26 @@ const getRegionHotels = async (req, res) => {
 const getRegionHotelBySeflink = async (req, res) => {
   const { seflink } = req.params;
 
-  const hotel = hotels.filter((h) => h.seflink === seflink);
+  const filteredHotels = hotels.filter((h) => h.seflink === seflink);
 
-  if (hotel) {
-    res.status(200).json(hotel);
+  const responseWithHotels = {
+    region: seflink,
+    numberOfHotels: filteredHotels.length,
+    hotels: filteredHotels,
+  };
+
+  const responseWithoutHotels = {
+    region: seflink,
+    numberOfHotels: 0,
+    hotels: [],
+  };
+
+  if (filteredHotels.length) {
+    res.status(200).json(responseWithHotels);
+  } else if (filteredHotels.length === 0) {
+    res.status(200).json(responseWithoutHotels);
   } else {
-    res.status(404).send({ message: 'Hotel not found.' });
+    res.status(404).send({ message: 'Aranan bölge/tema otelleri bulunamadı.' });
   }
 };
 
