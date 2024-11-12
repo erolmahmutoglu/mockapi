876bf65b0606ea2151a35811d5b8da83c45b1c95
Erol Mahmutoğlu <erol.mahmutoglu@outlook.com>
2024-11-12 15:51:26 +0300

    Update regionHotelsRouter.js

--- routes/regionHotelsRouter.js
+++ routes/regionHotelsRouter.js
@@ -8,6 +8,6 @@ const router = express.Router();
 router.use(addCorsHeader);
 
 router.route('/').get(regionHotelsController.getRegionHotels);
-router.route('/:seflink').get(regionHotelsController.getRegionHotelBySeflink);
+router.route('/:region').get(regionHotelsController.getRegionHotelByRegion);
 
 module.exports = router;
